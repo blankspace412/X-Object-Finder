9587022f2acd257635618a4e028db3c67609536d
Tasmiya Siddiqui <tasmiya.sid20@gmail.com>
2025-03-24 20:15:36 +0530

    login

--- app.py
+++ app.py
@@ -1,20 +1,75 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import bcrypt
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'secret'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            user = User.query.filter_by(email=email).first()
+            if user and user.check_password(password):
+                flash('Logged in successfully', 'success')
+                return redirect(url_for('index'))
+            else:
+                flash('Invalid credentials', 'danger')
+                return redirect(url_for('login'))
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        try:
+            user = User.query.filter_by(email=email).first() or User.query.filter_by(username=username).first()
+            if user:
+                flash('User already exists', 'danger')
+                return redirect(url_for('register'))
+        except:
+            pass
+
+        user = User(email, username, password)
+        db.session.add(user)
+        db.session.commit()
+        flash('User registered successfully', 'success')
+        return redirect(url_for('login'))
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
