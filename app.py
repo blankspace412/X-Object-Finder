350948c228f4514dbf9e2a93aff95338fa0b5d41
Tasmiya Siddiqui <tasmiya.sid20@gmail.com>
2025-04-06 19:10:21 +0530

    contact

--- app.py
+++ app.py
@@ -71,5 +71,16 @@ def register():
         return redirect(url_for('login'))
     return render_template('login.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
