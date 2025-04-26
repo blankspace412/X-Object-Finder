bdad1789f59d14f0ba863bc0d94c8e52d2027aee
Areeba Shakeel <ahmadshakeel5918@gmail.com>
2025-04-26 14:09:43 +0530

    ..

--- app.py
+++ app.py
@@ -32,6 +32,14 @@ class UploadForm(FlaskForm):
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
